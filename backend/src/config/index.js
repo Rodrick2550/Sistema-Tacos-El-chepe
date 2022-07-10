b4946a5abf246f46fb92134599e212e66e592184
Fernando Guerrero <devrrior@gmail.com>
2022-07-10 01:35:47 -0500

    style(backend): apply prettier

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,5 +1,14 @@
 import app from './main.js';
+import ConnectionDB from './database.js';
 
-app.listen(3000, () => {
+const connectionDB = new ConnectionDB();
+await connectionDB.createConnection();
+
+app.listen(3000, async () => {
+  connectionDB.connection.connect((err) => {
+    if (err) throw err;
+    console.log('Base de datos conectada');
+  });
+
   console.log(`Example app listening on port 3000`);
 });
