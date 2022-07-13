3a860ab416cca21e534308edc8051612a5faca13
Fernando Guerrero <devrrior@gmail.com>
2022-07-13 00:04:21 -0500

    feat(models): import sql files

--- backend/src/api/v1/models/index.js
+++ backend/src/api/v1/models/index.js
@@ -1,7 +1,24 @@
-import user from './user.model.js'
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-let queries = [];
+const __filename = fileURLToPath(import.meta.url);
 
-queries.push(user)
+const __dirname = path.dirname(__filename);
 
-export default queries;
+let tables = [];
+
+const cleanSQL = (sqlScript) => sqlScript.replace(/[\r\n]/gm, '');
+
+const tableTable = cleanSQL(fs.readFileSync(`${__dirname}/table.model.sql`).toString());
+tables.push(tableTable);
+const productTable = cleanSQL(fs.readFileSync(`${__dirname}/product.model.sql`).toString());
+tables.push(productTable);
+const userTable = cleanSQL(fs.readFileSync(`${__dirname}/user.model.sql`).toString());
+tables.push(userTable);
+const orderTable = cleanSQL(fs.readFileSync(`${__dirname}/order.model.sql`).toString());
+tables.push(orderTable);
+const orderItemTable = cleanSQL(fs.readFileSync(`${__dirname}/orderItem.model.sql`).toString());
+tables.push(orderItemTable);
+
+export default tables;
