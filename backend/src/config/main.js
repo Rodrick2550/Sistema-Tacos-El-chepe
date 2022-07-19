f748fbf8ecaae59d170c6d10c34c57df3fdc9ae2
Fernando Guerrero <devrrior@gmail.com>
2022-07-19 11:08:53 -0500

    feat(backend/config): add new routes to main

--- backend/src/config/main.js
+++ backend/src/config/main.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { application } from 'express';
 import userRoutes from '../api/v1/routes/user.route.js';
 import productRoutes from '../api/v1/routes/product.route.js';
+import orderRoutes from '../api/v1/routes/order.route.js';
+import orderItemRoutes from '../api/v1/routes/orderItem.route.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -17,6 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/orders', orderRoutes);
+app.use('/api/v1/order-items', orderItemRoutes);
 app.use('/images', express.static(path.join(__dirname, '../static/images')));
 
 export default app;
