75aa5cd7466092fd0354aaced971d09e3b586768
Fernando Guerrero <devrrior@gmail.com>
2022-07-18 21:08:49 -0500

    fix(backend/models): modify tables

--- backend/src/api/v1/models/order.model.sql
+++ backend/src/api/v1/models/order.model.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS orders (
   id_order INT PRIMARY KEY AUTO_INCREMENT,
   table_id_table INT NOT NULL,
   mesero_id_user INT NOT NULL,
-  total INT NOT NULL,
+  total INT NOT NULL DEFAULT 0,
   status ENUM('pending', 'completed') NOT NULL DEFAULT 'pending',
   CONSTRAINT fk_orders_tables FOREIGN KEY (table_id_table) REFERENCES tables (id_table),
   CONSTRAINT fk_orders_users FOREIGN KEY (mesero_id_user) REFERENCES users (id_usuario)
--- backend/src/api/v1/models/user.model.sql
+++ backend/src/api/v1/models/user.model.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
   id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(60) NOT NULL,
-  apellido VARCHAR(60) NOT NULL,
-  email VARCHAR(60) NOT NULL,
+  firstName VARCHAR(60) NOT NULL,
+  lastName VARCHAR(60) NOT NULL,
+  email VARCHAR(60) NOT NULL UNIQUE,
   password VARCHAR(60) NOT NULL,
   role ENUM('admin', 'waiter') NOT NULL DEFAULT 'waiter'
 );
