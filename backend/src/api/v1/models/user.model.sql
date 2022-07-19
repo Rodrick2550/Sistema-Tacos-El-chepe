f3ecaba73aa5c50f2ca5d8463a48c127da48743d
Fernando Guerrero <devrrior@gmail.com>
2022-07-18 21:13:06 -0500

    feat(backend/models/user): add chef option

--- backend/src/api/v1/models/user.model.sql
+++ backend/src/api/v1/models/user.model.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS users (
   lastName VARCHAR(60) NOT NULL,
   email VARCHAR(60) NOT NULL UNIQUE,
   password VARCHAR(60) NOT NULL,
-  role ENUM('admin', 'waiter') NOT NULL DEFAULT 'waiter'
+  role ENUM('admin', 'waiter', 'chef') NOT NULL DEFAULT 'waiter'
 );
