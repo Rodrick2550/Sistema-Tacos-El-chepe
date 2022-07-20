d58ac040d6aeda1b812b3d174084d40ec8a14af7
Fernando Guerrero <devrrior@gmail.com>
2022-07-20 00:52:01 -0500

    feat(backend/src/models/product): add category field

--- backend/src/api/v1/models/product.model.sql
+++ backend/src/api/v1/models/product.model.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS products (
   id_product INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(60) NOT NULL,
   description VARCHAR(60) NOT NULL,
+  category ENUM('taco', 'quesadilla', 'bebida') NOT NULL,
   price INT NOT NULL,
   image VARCHAR(255) NOT NULL
 );
