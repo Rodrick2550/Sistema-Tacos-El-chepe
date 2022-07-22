91394f83605bd411c88d55a7d84086c9ca67f8f7
Fernando Guerrero <devrrior@gmail.com>
2022-07-22 04:23:25 -0500

    feat(back/routes/orderItem): add controllers

--- backend/src/api/v1/routes/orderItem.route.js
+++ backend/src/api/v1/routes/orderItem.route.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { createOrderItemHandler, getOrderItemsHandler } from '../controllers/orderItem.controller.js';
+import {
+  createOrderItemHandler,
+  getOrderItemsHandler,
+} from '../controllers/orderItem.controller.js';
 
 const router = Router();
 
-router.post('/', createOrderItemHandler)
-  .get('/', getOrderItemsHandler);
+router
+  .post('/', createOrderItemHandler)
+  .get('/', getOrderItemsHandler)
 
 export default router;
