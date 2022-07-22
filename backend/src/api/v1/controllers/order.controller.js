596b0ee1a3382481b77ead059a7af719f793c4e0
Fernando Guerrero <devrrior@gmail.com>
2022-07-22 04:23:06 -0500

    feat(back/controllers/order): improve controllers

--- backend/src/api/v1/controllers/order.controller.js
+++ backend/src/api/v1/controllers/order.controller.js
@@ -24,15 +24,38 @@ const getOrdersHandler = async (req, res) => {
   const { id_mesero } = req.query;
 
   try {
+
     const orders = await connection.query(
-      `SELECT tables.name, orders.id_order FROM tables INNER JOIN orders ON tables.id_table = orders.table_id_table WHERE orders.mesero_id_user = ?`,
+      `SELECT tables.name, orders.id_order FROM tables INNER JOIN orders ON tables.id_table = orders.table_id_table WHERE orders.mesero_id_user = ? AND orders.status = 'pending'`,
       [id_mesero]
     );
 
     return res.status(200).json(orders[0]);
   } catch (e) {
+    console.log(e);
     return res.status(500).send();
   }
 };
 
-export { createOrderHandler, getOrdersHandler };
+const payOrderHandler = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await connection.query(
+      `UPDATE orders SET status = 'completed' WHERE id_order = ?`,
+      [id]
+    );
+
+    await connection.query(
+      `UPDATE tables SET status = 'available' WHERE id_table = (SELECT table_id_table FROM orders WHERE id_order = ?)`,
+      [id]
+    );
+
+    return res.status(200).send();
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send();
+  }
+};
+
+export { createOrderHandler, getOrdersHandler, payOrderHandler };
--- backend/src/api/v1/routes/order.route.js
+++ backend/src/api/v1/routes/order.route.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import {
   createOrderHandler,
   getOrdersHandler,
+  payOrderHandler,
 } from '../controllers/order.controller.js';
 
 const router = Router();
 
-router.post('/', createOrderHandler).get('/', getOrdersHandler);
+router.post('/', createOrderHandler).get('/', getOrdersHandler).post('/pay/:id', payOrderHandler);
 
 export default router;
