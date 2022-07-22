5c62c641e41a94c6f9202f9bd9190b35815c9bbb
Fernando Guerrero <devrrior@gmail.com>
2022-07-22 03:14:14 -0500

    fix(back/controllers/orderItem): update controller

--- backend/src/api/v1/controllers/orderItem.controller.js
+++ backend/src/api/v1/controllers/orderItem.controller.js
@@ -1,32 +1,48 @@
+import connection from "../../../config/database.js";
+
 const createOrderItemHandler = async (req, res) => {
   const { id_order, id_product, quantity } = req.body;
 
   try {
 
     // check if order item exists with the same product and the same order
-    const orderItems = await connection.query(
-      `SELECT * FROM order_items WHERE id_order = ? AND id_product = ?`,
+    const orderItem = await connection.query(
+      `SELECT * FROM order_items WHERE order_id_order = ? AND product_id_product = ?`,
       [id_order, id_product]
     );
 
-    if (orderItems[0].length > 0) {
-      return res.status(409).json({
-        message: 'El producto ya existe en la orden'
-      });
+    // get product price
+    const product = await connection.query(
+      `SELECT * FROM products WHERE id_product = ?`,
+      [id_product]
+    );
+
+    if (orderItem[0].length > 0) {
+      // update quantity and calculate total (price * quantity)
+      // string to int
+      const newQuantity = orderItem[0][0].quantity + parseInt(quantity);
+      const newTotal = newQuantity * product[0][0].price;
+
+      await connection.query(
+        `UPDATE order_items SET quantity = ?, total = ? WHERE order_id_order = ? AND product_id_product = ?`,
+        [newQuantity, newTotal, id_order, id_product]
+      );
+      return res.status(200).send();
     }
 
-    const orderItem = await connection.query(
-      `INSERT INTO order_items (id_order, id_product, quantity) VALUES (?, ?, ?)`,
-      [id_order, id_product, quantity]
+
+
+    // create new order item
+    await connection.query(
+      `INSERT INTO order_items (order_id_order, product_id_product, quantity, total) VALUES (?, ?, ?, ?)`,
+      [id_order, id_product, quantity, quantity * product[0][0].price]
     );
-    return res.status(201).json({
-      message: 'Orden creada correctamente'
-    });
+
+    return res.status(200).send();
   }
   catch (e) {
-    return res.status(500).json({
-      message: 'Error al crear la orden'
-    });
+    console.log(e);
+    return res.status(500).send();
   }
 }
 
